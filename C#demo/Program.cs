699dc08d451f82dcb7c7fc37dd6b0f0916f217b7
dong tran <duydong2108@gmail.com>
2024-08-05 21:16:04 +0700

    adding code

---
+++ C#demo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo{
+    class Program{
+        static void Main(string[] args){
+            float chieudai, chieurong;
+            Console.Write("Nhap vao chieu dai: ");
+            chieudai = float.Parse(Console.ReadLine());
+            Console.Write("Nhap vao chieu rong: ");
+            chieurong = float.Parse(Console.ReadLine());
+            Console.WriteLine("Dien tich hinh chu nhat la: " + chieudai*chieurong);
+        }
+    }
+}
