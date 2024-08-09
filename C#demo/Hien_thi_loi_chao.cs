bdf124d1ed30133737abb4053136473847ba1815
dong tran <duydong2108@gmail.com>
2024-08-09 18:03:21 +0700

    hoan thanh bt

---
+++ C#demo/Hien_thi_loi_chao.cs
@@ -0,0 +1,14 @@
+namespace Demo
+{
+    class ChuyenDoiTienTe
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
---
+++ C#demo/System_date.cs
@@ -0,0 +1,8 @@
+// namespace Demo{
+//     class ChuyenDoiTienTe{
+//         static void Main(string[] args){
+//             DateTime localDate = DateTime.Now;
+//             System.Console.WriteLine("Datetime Now is :" + localDate);
+//         }
+//     }
+// }
